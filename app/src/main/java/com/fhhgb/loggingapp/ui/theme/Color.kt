9ad9d5971e6d548ec0f3ccf108e7be8d24ff968d
TarikAnafarta <tarikanafarta@hotmail.com>
2025-11-19 23:26:14 +0300

    Refactor colors

--- app/src/main/java/com/fhhgb/loggingapp/ui/theme/Color.kt
+++ app/src/main/java/com/fhhgb/loggingapp/ui/theme/Color.kt
@@ -2,12 +2,11 @@ package com.fhhgb.loggingapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val backgroundColor = Color(0xFF79CE75)
-
+val tintColor = Color(0xFF6E6E8E)
+val darkGreen = Color(0xFF2E7D32)
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
